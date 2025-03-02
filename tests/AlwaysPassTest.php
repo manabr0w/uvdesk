d40b94f5b57a2e9ad002b781ef925e86b8338d68
maksim <maksimfedorenko71@gmail.com>
2025-03-02 16:16:41 +0200

    created test

---
+++ tests/AlwaysPassTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AlwaysPassTest extends TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
